b019d78d3f89ed9ff9ae1dbf920e12bed665ce56
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2019-03-04 14:27:00 +0000

    models and validations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_131037) do
+ActiveRecord::Schema.define(version: 2019_03_04_142125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "crawls", force: :cascade do |t|
+    t.string "start_location"
+    t.string "end_location"
+    t.integer "pub_number"
+    t.string "latitute"
+    t.string "longitude"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_crawls_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "user_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_03_04_131037) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "crawls", "users"
 end
