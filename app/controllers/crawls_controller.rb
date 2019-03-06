480ad8dd8da45c52c7a332fa395e1248a254d459
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2019-03-06 20:07:12 +0000

    get request directions set

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -1,9 +1,14 @@
+require 'httparty'
+
 class CrawlsController < ApplicationController
   def show
     @crawl = Crawl.find(params[:id])
     if @crawl.user != current_user
       redirect_to root_path, notice: "Not your crawl!"
     end
+
+    response = HTTParty.get('https://api.mapbox.com/directions/v5/mapbox/walking/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=pk.eyJ1IjoibWF4bG9uZ2JhbyIsImEiOiJjanN2cnVucjkwOWF0M3lwdjN2dG92cjB0In0.qK5tLU0Gzz2SVZgs8femMA')
+    raise
     @markers = [
       { lat: @crawl.start_latitude, lng: @crawl.start_longitude },
       { lat: @crawl.end_latitude, lng: @crawl.end_longitude }
---
+++ app/javascript/packs/navigation.js
@@ -0,0 +1,18 @@
+// import mapboxgl from 'mapbox-gl';
+// import mapboxgl from '@mapbox/mapbox-gl-directions';
+
+// var mapboxgl = require('mapbox-gl');
+// var MapboxDirections = require('@mapbox/mapbox-gl-directions');
+
+// var directions = new MapboxDirections({
+//   accessToken: 'MAPBOX_API_KEY=pk.eyJ1IjoibWF4bG9uZ2JhbyIsImEiOiJjanN2cnVucjkwOWF0M3lwdjN2dG92cjB0In0.qK5tLU0Gzz2SVZgs8femMA',
+//   unit: 'metric',
+//   profile: 'mapbox/cycling'
+// });
+
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v9'
+// });
+
+// map.addControl(directions, 'top-left');
