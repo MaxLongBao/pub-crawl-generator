c410b721132772a9df73097e98be2297249df0b0
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2019-03-12 23:23:35 +0000

    server

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -54,9 +54,11 @@ class CrawlsController < ApplicationController
           }
         end
         break if radius > 500
+        p @key_waypoints_pubs
       end
     end
-    @pub_lats = [[@crawl.start_longitude,@crawl.start_latitude]]
+      raise
+    @pub_lats = [[@crawl.start_longitude, @crawl.start_latitude]]
     @pub_markers.each { |marker| @pub_lats << [marker[:lng], marker[:lat]] }
     @pub_lats << [@crawl.end_longitude, @crawl.end_latitude]
     # @pubmarkers.each do |key,value|
